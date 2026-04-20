aa30940abca7a54ec55a07c1fb6b4378ed8fd20f
Samuel David Caro Borrero <sdavidcaro@unicesar.edu.co>
2026-04-19 23:16:52 -0500

    feat:proyecto RestauranteElBuenSabor

--- src/main/java/com/mycompany/restauranteelbuensabor/Proceso.java
+++ src/main/java/com/mycompany/restauranteelbuensabor/Proceso.java
@@ -5,42 +5,42 @@ public class Proceso {
     public static double CalcularTotalPedido() {
         double Subtotal = 0;
         double iva = 0;
-        double tot = 0;
-        double aux = 0;
-        int cont = 0;
+        double totalFactura = 0;
+        double SubtotalConDescuento = 0;
+        int CantidadITems = 0;
         int i = 0;
         while (i < Datos.nombreProductos.length) {
             if (Datos.cantidadPedida[i] > 0) {
                 Subtotal = Subtotal + Datos.precioProductos[i] * Datos.cantidadPedida[i];
-                cont = cont + 1;
+                CantidadITems = CantidadITems + 1;
             }
             i++;
         }
-        if (cont > 3) {
+        if (CantidadITems > 3) {
             if (Subtotal > 0) {
-                aux = Subtotal - (Subtotal * 0.05);
-                if (aux > 50000) {
-                    iva = aux * 0.19;
-                    tot = aux + iva;
-                    tot = tot + (tot * 0.10);
+                SubtotalConDescuento = Subtotal - (Subtotal * 0.05);
+                if (SubtotalConDescuento > 50000) {
+                    iva = SubtotalConDescuento * 0.19;
+                    totalFactura = SubtotalConDescuento + iva;
+                    totalFactura = totalFactura + (totalFactura * 0.10);
                 } else {
-                    iva = aux * 0.19;
-                    tot = aux + iva;
+                    iva = SubtotalConDescuento * 0.19;
+                    totalFactura = SubtotalConDescuento + iva;
                 }
             }
         } else {
             if (Subtotal > 50000) {
                 iva = Subtotal * 0.19;
-                tot = Subtotal + iva;
-                tot = tot + (tot * 0.10);
+                totalFactura = Subtotal + iva;
+                totalFactura = totalFactura + (totalFactura * 0.10);
             } else {
                 iva = Subtotal * 0.19;
-                tot = Subtotal + iva;
+                totalFactura = Subtotal + iva;
             }
         }
         Datos.estadoMesa = 1;
-        Datos.totalFactura = tot;
-        return tot;
+        Datos.totalFactura = totalFactura;
+        return totalFactura;
     }
 
     public static double procesar(double a, double b, double c, double d, double e, int f, boolean g) {
